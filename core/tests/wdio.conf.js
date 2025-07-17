96af2e73a105122cdc0e23f29d0ef9dc4bf64e8b
Jayam Patel <jayampatel04@gmail.com>
2025-07-17 09:38:55 +0530

    blocked popups globally.

--- core/tests/wdio.conf.js
+++ core/tests/wdio.conf.js
@@ -199,6 +199,7 @@ exports.config = {
         '--no-sandbox',
         '--disable-dev-shm-usage',
         '--disable-gpu',
+        '--disable-popup-blocking'
       ],
       prefs: {
         download: {
@@ -217,7 +218,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent.
-  logLevel: 'warn',
+  logLevel: 'info',
 
   // Set a base URL in order to shorten url command calls. If your `url`
   // parameter starts with `/`, the base url gets prepended, not including
