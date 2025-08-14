cf09fcff5db28ef5e4e2e607b9cfbd7bd5aad34d
Jayam Patel <jayampatel04@gmail.com>
2025-08-15 00:19:47 +0530

    Minor fix.

--- core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
@@ -5042,6 +5042,7 @@ export class ExplorationEditor extends BaseUser {
 
     await this.clickOn(addManualVoiceoverButton);
     await this.uploadFile(voiceoverFilePath);
+    await this.waitForElementToStabilize(saveUploadedAudioButton);
     await this.clickOn(saveUploadedAudioButton);
     await this.waitForNetworkIdle();
 
