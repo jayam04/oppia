6a6acf1e03b6bcb2729af3f7fafc7b37b09a8cee
Jayam Patel <jayampatel04@gmail.com>
2025-08-19 15:21:34 +0530

    Lint fixes

--- core/tests/puppeteer-acceptance-tests/utilities/user/curriculum-admin.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/curriculum-admin.ts
@@ -71,7 +71,6 @@ const submitSolutionButton = 'button.e2e-test-submit-solution-button';
 const saveQuestionButton = 'button.e2e-test-save-question-button';
 
 const dismissWelcomeModalSelector = 'button.e2e-test-dismiss-welcome-modal';
-const dropdownToggleIcon = '.e2e-test-mobile-options-dropdown';
 
 const topicsTab = 'a.e2e-test-topics-tab';
 const desktopTopicSelector = 'a.e2e-test-topic-name';
