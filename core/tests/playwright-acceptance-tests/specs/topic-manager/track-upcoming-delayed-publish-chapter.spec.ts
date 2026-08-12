64679af5ccae03661ec5ea9f108c2dda6e9e5631
Rusty Astromech <rustyastromech@jayampatel.space>
2026-08-12 03:48:26 +0000

    Fix beforeAll timeout in track-upcoming-delayed-publish-chapter spec

--- core/tests/playwright-acceptance-tests/specs/topic-manager/track-upcoming-delayed-publish-chapter.spec.ts
+++ core/tests/playwright-acceptance-tests/specs/topic-manager/track-upcoming-delayed-publish-chapter.spec.ts
@@ -48,13 +48,12 @@ const publishUptoChaptersDropdownSelector =
 test.describe.configure({mode: 'serial'});
 
 test.describe('Logged-In Learner', function () {
+  test.setTimeout(1200000);
   let curriculumAdmin: CurriculumAdmin & ExplorationEditor & TopicManager;
   let releaseCoordinator: ReleaseCoordinator;
   const chapterIds: string[] = [];
 
-  test.beforeAll(async function ({browser}) {
-    test.setTimeout(6000000);
-
+  test.beforeAll({timeout: 1200000}, async function ({browser}) {
     curriculumAdmin = await UserFactory.createNewUser(
       'curriculumAdm',
       'curriculumAdmin@example.com',
